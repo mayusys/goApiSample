b9a3876a9dfafcac392e5a629f8315cb24ce5b79
Mayu Yamamoto <d.s.v.w.y0106@gmail.com>
2017-11-12 15:30:20 +0900

    fix

--- main.go
+++ main.go
@@ -13,26 +13,54 @@ type Todo struct {
 	CompleteFlg bool   `json:"completeflg,omitempty"`
 }
 
-var todo []Todo
+var todos []Todo
 
 func main() {
 	router := mux.NewRouter()
 
-	todo = append(todo, Todo{ID: "1", TaskName: "Learn Programming", CompleteFlg: false})
-	todo = append(todo, Todo{ID: "2", TaskName: "Diet", CompleteFlg: true})
-	todo = append(todo, Todo{ID: "3", TaskName: "Play the Violin", CompleteFlg: false})
+	todos = append(todos, Todo{ID: "1", TaskName: "Learn Programming", CompleteFlg: false})
+	todos = append(todos, Todo{ID: "2", TaskName: "Diet", CompleteFlg: true})
+	todos = append(todos, Todo{ID: "3", TaskName: "Play the Violin", CompleteFlg: false})
 
-	router.HandleFunc("/todo", GetTodo).Methods("GET")
+	router.HandleFunc("/todo", GetTodos).Methods("GET")
 	router.HandleFunc("/todo/{id}", GetTodo).Methods("GET")
 	router.HandleFunc("/todo/{id}", CreateTodo).Methods("POST")
 	router.HandleFunc("/todo/{id}", DeleteTodo).Methods("DELETE")
 	log.Fatal(http.ListenAndServe(":8000", router))
 }
 
-func GetTodo(w http.ResponseWriter, r *http.Request) {
-	json.NewEncoder(w).Encode(todo)
+func GetTodos(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode(todos)
 }
 
-func GetTodos(w http.ResponseWriter, r *http.Request)   {}
-func CreateTodo(w http.ResponseWriter, r *http.Request) {}
-func DeleteTodo(w http.ResponseWriter, r *http.Request) {}
+func GetTodo(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	for _, item := range todos {
+		if item.ID == params["id"] {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+}
+func CreateTodo(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	var todo Todo
+	_ = json.NewDecoder(r.Body).Decode(&todo)
+
+	todo.ID = params["id"]
+	todos = append(todos, todo)
+	json.NewEncoder(w).Encode(todos)
+}
+
+func DeleteTodo(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	for index, item := range todos {
+		if item.ID == params["id"] {
+			todos = append(todos[:index], todos[index+1:]...)
+			newTodo := make([]Todo, len(todos))
+			copy(newTodo, todos)
+			break
+		}
+	}
+	json.NewEncoder(w).Encode(todos)
+}
